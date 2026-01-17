1d350298538f24343fd9cd65d32693370724fa89
77littlebox77 <gui.caixinha77@gmail.com>
2026-01-17 18:08:42 +0000

    Configuração final da base de dados

--- meusite/settings.py
+++ meusite/settings.py
@@ -81,14 +81,13 @@ WSGI_APPLICATION = 'meusite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
     'default': dj_database_url.parse(
         'postgresql://postgres.njombjtatyitumivuijo:mangastraquinas@aws-1-eu-west-1.pooler.supabase.com:6543/postgres'
-        conn_max_age=600,
-        ssl_require=True
     )
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
