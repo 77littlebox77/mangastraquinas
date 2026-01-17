f05caca471eabc51cec9ef51ed4626df7ad9783f
77littlebox77 <gui.caixinha77@gmail.com>
2026-01-17 17:48:15 +0000

    Corrigir pasta meusite no vercel

--- meusite/wsgi.py
+++ meusite/wsgi.py
@@ -10,8 +10,7 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MangasTraquinas.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meusite.settings')
 application = get_wsgi_application()
 
 app = application # <--- ADICIONA ISTO NO FIM
